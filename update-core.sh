1463f930e53c23d44300dd93bd14b5373668a6cb
Maxim Oransky <maxim.oransky@gmail.com>
2018-04-09 04:43:57 +0300

    фиксы всего

--- update-core.sh
+++ update-core.sh
@@ -3,4 +3,4 @@ set -e
 cd `dirname $0`
 
 cp -fv futubank-library-php/v2/fpayments.php wordpress/modulbank-wordpress-shortcode/modulbank-shortcode/inc/fpayments.php
-cp -fv futubank-library-php/v2/fpayments.php wordpress/modulbank-woocommerce/modulbank/inc/fpayments.php
+cp -fv futubank-library-php/v2/fpayments.php woocommerce/modulbank-woocommerce/modulbank/inc/fpayments.php
--- woocommerce/docker-entrypoint.sh
+++ woocommerce/docker-entrypoint.sh
@@ -6,6 +6,6 @@ rsync -a /opt/woocommerce /var/www
 cd /var/www/woocommerce
 
 wp plugin --allow-root activate woocommerce
-wp plugin --allow-root activate modulbank
+#wp plugin --allow-root activate modulbank
 
 exec $@
